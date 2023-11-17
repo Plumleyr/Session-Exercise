e7fe1d22875ab43c0cb773fbcd18d02360c8af12
Robin Jacob Plumley <jrplumley@gmail.com>
2023-11-16 23:12:28 -0500

    Adding incase I break something

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, url_for
+from flask import Flask, render_template, redirect, request, url_for, flash
 
 from flask_debugtoolbar import DebugToolbarExtension
 
@@ -22,20 +22,23 @@ def show_survey():
     return render_template('survey.html', satisfaction_survey = satisfaction_survey)
 
 @app.route('/question/<int:question_number>', methods = ['POST', 'GET'])
-def show_questions(question_number):
-    print(responses)
-    if request.method == 'POST':
-        responses.append(request.form['choice'])
-        question_number += 1
-        if question_number < len(satisfaction_survey.questions):
-            return redirect(url_for('show_questions', question_number = question_number))
-        
-    return render_template('questions.html', question_number = question_number, satisfaction_survey = satisfaction_survey)
+def show_questions(question_number=0):
+    if question_number != len(responses):
+        flash("Invalid link, redirected to correct question.")
+        return redirect(url_for('show_questions', question_number = len(responses)))
+    else:
+        return render_template('questions.html', question_number = question_number, satisfaction_survey = satisfaction_survey)
+
+@app.route('/thank_you')
+def show_thanks():
+    return render_template('thank_you.html')
 
 @app.route('/answer', methods = ['POST', 'GET'])
 def show_answers():
     responses.append(request.form['choice'])
-    print(responses)
     question_number = int(request.args.get('question_number')) + 1
     if question_number < len(satisfaction_survey.questions):
         return redirect(url_for('show_questions', question_number = question_number))
+    else:
+        return redirect('/thank_you')
+
